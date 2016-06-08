10db3b814812ac6136173e08cca99bfae2807885
Matheus Azzi <matheuslazzi@gmail.com>
2016-06-08 17:19:17 -0300

    Limit beers on buffer

--- README.md
+++ README.md
@@ -4,11 +4,7 @@ Producer's job is to generate data (a beer), put into the buffer (a fixed-size q
 
 Consumers will ask for beer when they are available and Producers will be sending beers they made.
 
-You can have multiple Producers and Consumers running, you must ensure that all Consumers drink in request order and Producers stop to make beers if the buffer is full.
-
-#### TODO
-- [ ] Add a limit to beers list (buffer)
-- [ ] Make producers stop to make beers if list is full
+You can have multiple Producers and Consumers running, you must ensure that all Consumers drink in request order and don't put beer into buffer if the buffer is full.
 
 ### Dependencies
 To run this project you must have [Elixir](http://elixir-lang.org/install.html) and [Mix](http://elixir-lang.org/getting-started/mix-otp/introduction-to-mix.html) installed on your machine.
--- lib/manager.ex
+++ lib/manager.ex
@@ -2,49 +2,65 @@ defmodule Manager do
   def start(producers: num_producers, consumers: num_consumers, buffer_size: buffer_size) do
     Enum.map(1..num_consumers, fn _ -> Consumer.start(self) end)
     Enum.each(1..num_producers, fn _ -> Producer.start(self) end)
-    loop([], [])
+    loop([], [], buffer_size)
   end
 
-  defp loop(beers, waiting_consumers) do
+  defp loop(beers, waiting_consumers, buffer_size) do
     receive do
       {:request, consumer_pid} ->
         IO.puts "[M] Consumidor #{inspect consumer_pid} pediu uma cerveja."
-        send_beer(beers, consumer_pid, waiting_consumers)
+        send_beer(beers, consumer_pid, waiting_consumers, buffer_size)
 
       {:beer, beer, producer_pid} ->
         IO.puts "[M] Cerveja ##{beer} recebida do produtor #{inspect producer_pid}."
-        receive_beer(beers, {:beer, beer, producer_pid}, waiting_consumers)
+
+        if length(beers) >= buffer_size do
+          list_is_full(beers, beer, waiting_consumers, buffer_size)
+        else
+          receive_beer(beers, {:beer, beer, producer_pid}, waiting_consumers, buffer_size)
+        end
     end
   end
 
-  defp send_beer([], consumer_pid, waiting_consumers) do
+  # No beers
+  defp send_beer([], consumer_pid, waiting_consumers, buffer_size) do
     IO.puts "[M] Sem cervejas na fila. Consumidor #{inspect consumer_pid} vai esperar próxima."
-    print_beers_list([])
-    loop([], waiting_consumers ++ [consumer_pid])
+    print_beers_list([], buffer_size)
+    loop([], waiting_consumers ++ [consumer_pid], buffer_size)
   end
 
-  defp send_beer(beers, consumer_pid, waiting_consumers) do
+  # Has beers
+  defp send_beer(beers, consumer_pid, waiting_consumers, buffer_size) do
     first_beer = hd(beers)
     IO.puts "[M] Cerveja ##{elem(first_beer, 1)} enviada ao consumidor #{inspect consumer_pid}."
     send(consumer_pid, first_beer)
 
-    print_beers_list(tl(beers))
-    loop(tl(beers), waiting_consumers)
+    print_beers_list(tl(beers), buffer_size)
+    loop(tl(beers), waiting_consumers, buffer_size)
   end
 
-  defp receive_beer(beers, beer, []) do
+  # No waiting consumers
+  defp receive_beer(beers, beer, [], buffer_size) do
     beers = beers ++ [beer]
     IO.puts "[M] Cerveja ##{elem(beer, 1)} colocada na fila."
-    print_beers_list(beers)
-    loop(beers, [])
+    print_beers_list(beers, buffer_size)
+    loop(beers, [], buffer_size)
   end
 
-  defp receive_beer(beers, beer, waiting_consumers) do
-    send_beer(beers ++ [beer], hd(waiting_consumers), tl(waiting_consumers))
+  # There are waiting consumers
+  defp receive_beer(beers, beer, waiting_consumers, buffer_size) do
+    send_beer(beers ++ [beer], hd(waiting_consumers), tl(waiting_consumers), buffer_size)
   end
 
-  defp print_beers_list(beers) do
+  # Buffer is full
+  defp list_is_full(beers, beer, waiting_consumers, buffer_size) do
+    IO.puts "[M] Fila de cervejas já está cheia. Cerveja ##{beer} descartada."
+    print_beers_list(beers, buffer_size)
+    loop(beers, waiting_consumers, buffer_size)
+  end
+
+  defp print_beers_list(beers, buffer_size) do
     printable_list = Enum.map(beers, fn(beer) -> elem(beer, 1) end)
-    IO.puts ['Cervejas: ', inspect(printable_list, char_lists: :as_lists)]
+    IO.puts ['Cervejas: ', inspect(printable_list, char_lists: :as_lists), " Qtd: #{length(beers)}/#{buffer_size}"]
   end
 end
